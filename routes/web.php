fc8f37c218183dfaebfc4b79033ab8930f1cbaf3
Jhih Luo <losididog@gmail.com>
2024-07-10 14:25:38 +0800

    新增db seeder, factory

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
+// All listings
 Route::get('/', function () {
-    return view('welcome');
+  return view('listings', [
+    'heading' => 'Latest Listings',
+    'listings' => Listing::all(),
+  ]);
+});
+
+Route::get('/hello', function () {
+  return response('<h1>Hello World</h1>')
+    ->header('Content-Type', 'text/plain')
+    ->header('foo', 'bar');
+});
+
+// Single listing
+Route::get('/listings/{id}', function ($id) {
+  return view('listing', [
+    'listing' => Listing::find($id),
+  ]);
 });
